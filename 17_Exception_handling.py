3b664f88d0758561c28ca7aaec345bdfa1f7b336
Nicolas Luévano <nicolas.luevano@chownow.com>
2024-01-25 09:35:53 -0800

    Day 17, 21, 22

---
+++ 17_Exception_handling.py
@@ -0,0 +1,10 @@
+
+# Unpack the first five countries and store them in a variable nordic_countries, store Estonia and Russia in es, and ru respectively.
+
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+
+
+*nordic_countries, es, ru = names
+print(nordic_countries, es, ru)
+
+
