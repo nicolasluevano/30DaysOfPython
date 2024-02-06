f9f8d90c30ba5d116362163169eed4f2c1c0b74d
Nicolas Luevano <nluevano1221@gmail.com>
2024-02-05 16:31:57 -0800

    Add API endpoint to retrieve students data

--- python_for_web/app.py
+++ python_for_web/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@localhost/thirty_days_of_python'
@@ -12,6 +13,36 @@ db = SQLAlchemy(app)
 
 app.app_context().push()
 
+
+#create Models direcotry and add all models to it
+class Students(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    country = db.Column(db.String())
+    city = db.Column(db.String())
+    age = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f"Student('{self.name}', '{self.country}, '{self.city}', '{self.age}')"
+
+
+@app.route('/api/v1.0/students', methods=['GET'])
+def get_students():
+    students = Students.query.all()
+    students_list = []
+
+    for student in students:
+        student_data = {
+            'id': student.id,
+            'name': student.name,
+            'country': student.country,
+            'city': student.city,
+            'age': student.age
+        }
+        students_list.append(student_data)
+
+    return Response(json.dumps(students_list), mimetype='application/json')
+    # return jsonify([student.to_json() for student in students])
 
 @app.route('/')
 def home():
@@ -49,9 +80,20 @@ def result():
                     count = current_frequency
                     freq_word = word
             return freq_word
+        
+        def word_dict(words):
+            word_dict = {}
+            for word in words:
+                if word not in word_dict:
+                    word_dict[word] = 1
+                elif word in word_dict:
+                    word_dict[word] += 1
+            sorted_dict = dict(sorted(word_dict.items(), key=lambda x:x[1], reverse=True))
+            return sorted_dict
 
         total_letters = letter_count(content)
         most_freq_word = most_frequent_word(words)
+        sorted_dict = word_dict(words)
 
         return render_template(
             'result.html',
@@ -59,7 +101,8 @@ def result():
             content_length=content_length,
             total_letters=total_letters,
             most_freq_word=most_freq_word,
-            word_set=word_set
+            word_set=word_set,
+            sorted_dict=sorted_dict
         )
 
 
--- python_for_web/create_db.py
+++ python_for_web/create_db.py
@@ -4,14 +4,24 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="password123"
+    passwd="password123",
+    database="thirty_days_of_python"
 )
 
 my_cursor = mydb.cursor()
 
 # my_cursor.execute("CREATE DATABASE thirty_days_of_python")
+# my_cursor.execute("CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255),country VARCHAR(255),city VARCHAR(255),age VARCHAR(255))")
+# my_cursor.execute("ALTER TABLE students ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+# my_cursor.execute("DROP TABLE students")
 
-my_cursor.execute("SHOW DATABASES")
+# my_cursor.execute("SHOW DATABASES")
+# my_cursor.execute("SHOW TABLES")
+my_cursor.execute("SELECT * from students")
+result = my_cursor.fetchall() 
 
-for db in my_cursor:
-    print(db)
+# for db in my_cursor:
+#     print(db)
+
+for row in result: 
+    print(row, '\n') 
